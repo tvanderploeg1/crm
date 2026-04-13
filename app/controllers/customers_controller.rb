7479e4c7cc2c41f7e39dc852414b30a1c00a9e9a
TVanderploeg1 <tiffany.j.v.5@gmail.com>
2026-04-13 09:36:01 -0500

    Implement customers index with partial and navigation links

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,13 @@
 class CustomersController < ApplicationController
   def index
+    @customers = Customer.all.with_attached_image
   end
 
   def alphabetized
+    @customers = Customer.all.with_attached_image.order(full_name: :asc)
   end
 
   def missing_email
+    @customers = Customer.all.with_attached_image.where(email_address: nil)
   end
 end
